2f793e229730eaef8cfa2352defb64442ac11b8d
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-07-29 00:04:48 -0500

    Added function selection, this function prompts user to select rock, paper, or scissors

--- script.js
+++ script.js
@@ -5,11 +5,39 @@ function computerPlay() {
     let pick = randomNumber();
     // If the pick is 0 return "Rock",if 1 return "Paper" if 2 return "Scissors"
     if (pick === 0) {
-        return "Rock"
+        return "rock"
     } else if (pick === 2) {
-        return "Paper"
+        return "raper"
     } else {
-        return "Scissors"
+        return "scissors"
     }
 }
 
+//selection function prompts users to enter rock, paper, or scissors. if the input is not rock, paper, scissors the user is prompted again until the correct input is entered
+function selection() {
+    //flag for while loop, as long as selected is false it will repeat
+    let selected = false;
+    while (selected === false) {
+        // prompt user for input
+        let playerSelection = prompt("Rock, Paper, or Scissors?").toLocaleLowerCase();
+        //checks user input for rock paper or scissors, and returns matched result
+        if (playerSelection === "rock") {
+            return "rock";
+        } else if (playerSelection === "paper") {
+            return "paper"
+        } else if (playerSelection === "scissors") {
+            return "scissors"
+        } else {
+            //if no result is matched alert user and repeat loop.    
+            alert("Please enter rock paper or scissors")
+        }
+    }
+}
+
+//playRound functions takes the user input and plays a round of rock, paper, scissors with computer
+
+function playRound(playerSelection, computerSelection) {
+
+}
+
+console.log(selection());
