73b6cabbd5067bd41c9924bed278d704eccf6ec1
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-07-28 23:43:05 -0500

    Added function computerPlay, this function generates a string randomly that is rock, paper, or scissors

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//
+//computerPlay function randomly generates "Rock", "Paper", or "Scissors"
+function computerPlay() {
+    // Generate a random number from 0 to 2       
+    let randomNumber = () => Math.floor(Math.random() * (3));
+    let pick = randomNumber();
+    // If the pick is 0 return "Rock",if 1 return "Paper" if 2 return "Scissors"
+    if (pick === 0) {
+        return "Rock"
+    } else if (pick === 2) {
+        return "Paper"
+    } else {
+        return "Scissors"
+    }
+}
+
